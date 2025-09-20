378e7c5a96e8d5c19da03953dae11d01b5d8da25
Maaz khan <maaz13888@gmail.con>
2025-09-20 16:57:07 +0500

    work on frist day

---
+++ variable.js
@@ -0,0 +1,28 @@
+// const acountID = "123456"
+// let name = "John Doe"
+// var section = null
+// // console.log([acountID,name,section])
+// /*
+// yi simple hamari pass just aik work ta 
+// */
+// // ab ham us ki bata type ki bari mi bat kari gi 
+// console.log(typeof section)
+// let valueInnumber = Number(section)
+// console.log(typeof (valueInnumber))
+// let valueInnumberS = String (valueInnumber)
+// console.log(valueInnumberS) 
+
+
+
+//  conversion of data type
+
+// let myName 
+// let changeName = Boolean(myName)
+// console.log(typeof changeName)
+// console.log( changeName)
+
+
+
+//  camparion of datatypes
+ console.log("21");
+ 
